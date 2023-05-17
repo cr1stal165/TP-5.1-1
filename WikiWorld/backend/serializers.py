d053547559bd4dd9c1d80d39db0ad3cd35f5c889
Sergey Silnov <loginpasword2017@gmail.com>
2023-05-17 18:38:38 +0300

    edit srializers for article

--- WikiWorld/backend/serializers.py
+++ WikiWorld/backend/serializers.py
@@ -3,22 +3,26 @@ from rest_framework import serializers
 from backend.models import Article, User, Topic
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class TopicSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Topic
-        fields = '__all__'
-
-
 class ArticleSerializer(serializers.ModelSerializer):
+    date = serializers.DateField(read_only=True)
+    user = serializers.CurrentUserDefault()
 
     class Meta:
         model = Article
-        fields = '__all__'
+        fields = "__all__"
+
+
+class TopicSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Topic
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('nickname',)
 
 
